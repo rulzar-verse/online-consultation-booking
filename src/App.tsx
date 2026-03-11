462b3381eb98c3f797e5feddfa5e5d9e338454b9
rulzar-verse <rulzar.earther@gmail.com>
2026-03-12 01:44:35 +0600

    Initial commit: Add Dr. Samiul Kafi Siam consultation booking app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Doctor Consultation Booking</h1>
+            {/* Add booking component here */}
+        </div>
+    );
+};
+
+export default App;
